da1bb080c2af4f96bb5c369ff29d0158159ef71b
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-06-05 22:41:40 +0100

    First Commit - Folder Structure Setup

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'src/app.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+    ),
+  );
+
+  runApp(App());
+}
---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'ui/home_screen.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      title: 'Auth',
+      home: HomeScreen(),
+    );
+  }
+}
